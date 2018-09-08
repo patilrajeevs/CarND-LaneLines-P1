09962a548d161b388125ac74b02c3b8d16baae23
patilrajeevs <patilrajeevs@yahoo.com>
2018-09-08 22:09:21 +0530

    Group into two lines using slope/intercept
    1. This patch enables identification of lane lines.
    2. Group of 2 lines with slope and intercept using kmeans.
    3. Identify the average of the lines
    4. Logic 2 requires removal of outliers, which is not done currently.
       This leads to incorrect averaging as extra un-wanted lines are
       present

--- lane_lines.cpp
+++ lane_lines.cpp
@@ -1,14 +1,17 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
-#include "../utility/opencv/helper.cpp"
-#include <iostream>
 #include <opencv2/opencv.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
+#include <cv.h>
 #include <vector>
+#include <iostream>
+
+#include "../utility/opencv/opencv_helper.h"
+#include "../utility/geometry/geometry.h"
 
 using namespace cv;
 using namespace std;
-
 
 void pipeline(Mat &image){
   Size img_size = image.size();
@@ -27,7 +30,7 @@ void pipeline(Mat &image){
   //OpencvHelper::display(play_image, "temp_window");
   play_image =  OpencvHelper::canny_edge(play_image, 50, 3);
   play_image = OpencvHelper::getRoI(play_image, roi_pts);
-  OpencvHelper::display(play_image, "temp_window");
+  //OpencvHelper::display(play_image, "temp_window");
   //OpencvHelper::display(mask_image, "temp_window");
   //OpencvHelper::ApplyMask(bw_image, color_mask);
   double rho = 2;
@@ -36,7 +39,29 @@ void pipeline(Mat &image){
   double minLineLength = 40;
   double maxLineGap = 20;
   vector<Vec4i> lines = OpencvHelper::GetHoughLines(play_image,rho, theta, threshold, minLineLength, maxLineGap, true);
-  
+  vector<Line> identified_lines;
+  for (vector<Vec4i>::iterator it = lines.begin(); it != lines.end(); ++it){
+    identified_lines.push_back(Line(*it));
+  }
+  map <int, vector <Line> > line_groups;
+  Geometry::GroupLinesBySlopeIntercept(identified_lines, line_groups, 2);
+  for (map <int, vector <Line> >::iterator it = line_groups.begin(); it != line_groups.end(); ++it){
+      Line averaged_line = Geometry::AverageLines(it->second);
+      averaged_line.py1 = img_size.height;
+      averaged_line.py2 = img_size.height/2;
+      averaged_line.px1 = averaged_line.GetXInterceptAt(averaged_line.py1);
+      averaged_line.px2 = averaged_line.GetXInterceptAt(averaged_line.py2);
+      line(image, Point(averaged_line.px1,averaged_line.py1), Point(averaged_line.px2,averaged_line.py2),CvScalar(0,255,0));
+  /*
+      CvScalar color(0,255,0);
+      vector <Line> temp_vector = it->second;
+      for (vector<Line>::iterator it_line = temp_vector.begin(); it_line != temp_vector.end(); ++it_line){
+        line(image, Point((*it_line).px1,(*it_line).py1), Point((*it_line).px2,(*it_line).py2),CvScalar(0,255,0));
+        color = (0,0,255);
+      }
+    */
+  }
+  OpencvHelper::display(image, "temp_window");
 }
 
 int main(){
