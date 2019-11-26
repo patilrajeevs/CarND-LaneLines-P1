c1a1b6eb1a19c3907d86452a36d784974d7b8bc7
patilrajeevs <patilrajeevs@yahoo.com>
2019-11-26 19:53:39 +0530

    Enable compiling using cmake
    Now requires download-build-install of the utility library as a
    pre-requsite
    https://github.com/patilrajeevs/utility

--- src/lane_lines.cpp
+++ src/lane_lines.cpp
@@ -2,18 +2,18 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/core/types_c.h>
 
-#include <cv.h>
 #include <vector>
 #include <iostream>
 
-#include "../utility/opencv/opencv_helper.h"
-#include "../utility/geometry/geometry.h"
+#include <opencv_helper.h>
+#include <geometry_helper.h>
 
 using namespace cv;
 using namespace std;
 
-void pipeline(Mat &image){
+Mat pipeline(Mat &image){
   Size img_size = image.size();
   Point p_1(0, img_size.height);
   Point p_2(img_size.width/2,img_size.height/2); 
@@ -38,7 +38,7 @@ void pipeline(Mat &image){
   int threshold = 15;
   double minLineLength = 40;
   double maxLineGap = 20;
-  vector<Vec4i> lines = OpencvHelper::GetHoughLines(play_image,rho, theta, threshold, minLineLength, maxLineGap, true);
+  vector<Vec4i> lines = OpencvHelper::GetHoughLines(play_image,rho, theta, threshold, minLineLength, maxLineGap, false);
   vector<Line> identified_lines;
   for (vector<Vec4i>::iterator it = lines.begin(); it != lines.end(); ++it){
     identified_lines.push_back(Line(*it));
@@ -51,7 +51,7 @@ void pipeline(Mat &image){
       averaged_line.py2 = img_size.height/2;
       averaged_line.px1 = averaged_line.GetXInterceptAt(averaged_line.py1);
       averaged_line.px2 = averaged_line.GetXInterceptAt(averaged_line.py2);
-      line(image, Point(averaged_line.px1,averaged_line.py1), Point(averaged_line.px2,averaged_line.py2),CvScalar(0,255,0));
+      line(image, Point(averaged_line.px1,averaged_line.py1), Point(averaged_line.px2,averaged_line.py2),(0,255,0));
   /*
       CvScalar color(0,255,0);
       vector <Line> temp_vector = it->second;
@@ -59,13 +59,16 @@ void pipeline(Mat &image){
         line(image, Point((*it_line).px1,(*it_line).py1), Point((*it_line).px2,(*it_line).py2),CvScalar(0,255,0));
         color = (0,0,255);
       }
-    */
+  */
   }
   OpencvHelper::display(image, "temp_window");
+  return image;
 }
 
+
 int main(){
-  Mat image = imread("test_images/solidWhiteCurve.jpg");
+  Mat image = imread("../test_images/solidWhiteCurve.jpg");
+  OpencvHelper obj;
   pipeline(image);
   return 0;
 }
